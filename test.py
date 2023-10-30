7943027fbbf2a211a3c2781b3e023cb534735089
Sarim Khan <sarimkhan2work@gmail.com>
2023-10-30 16:15:17 -0500

    Update test.py

--- test.py
+++ test.py
@@ -12,6 +12,6 @@ class Item(BaseModel):
 
 @app.get("/item/{itemId}")
 def read_root():
-    post = fetch.get_listing_details('c8993477-60b7-3735-826f-8111b9a9df22')
+    post = fetch.get_listing_details(itemId)
     item = Item(title =post['data']['listing']['originalTitle'], price = post['data']['listing']['originalPrice'], pics = post['data']['listing']['photos'], description = post['data']['listing']['description'])
-    return item.model_dump_json() 
+    return item.model_dump_json()
