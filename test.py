b1cdedd9d260364f06baf34801be3abd7ec63357
Sarim Khan <sarimkhan2work@gmail.com>
2023-10-30 20:43:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,20 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app = FastAPI()
 class Item(BaseModel):
     title: str
