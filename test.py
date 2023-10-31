e327cac8835b926abbb782e2fcea79559b8b8218
Sarim Khan <sarimkhan2work@gmail.com>
2023-10-31 00:28:11 +0000

    jj

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class Item(BaseModel):
     description: str
 
 @app.get("/item/{itemId}")
-def read_root():
+def read_root(itemId):
     post = fetch.get_listing_details(itemId)
     item = Item(title =post['data']['listing']['originalTitle'], price = post['data']['listing']['originalPrice'], pics = post['data']['listing']['photos'], description = post['data']['listing']['description'])
-    return item.model_dump_json()
+    return item.model_dump_json() 
